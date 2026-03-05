6689a8b27c3695f6c8bf7fb0812d034e2459b506
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-03-05 07:17:31 -0300

    Iniciando com javascript

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+async function loginFetch(){
+
+    const email = document.getElementById("email_input").value.toLowerCase();
+    const pass = document.getElementById("pass_input").value
+
+    if (!(email === "" || pass.value === "")){
+        const response = await fetch("http://localhost:8080")
+    }
+}
