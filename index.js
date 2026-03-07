36a793dfafc2b078ddc526075a4dab3c2dc05da2
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-03-07 07:02:44 -0300

    Dando continuidade no fetch

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 async function loginFetch(){
 
     const email = document.getElementById("email_input").value.toLowerCase();
-    const pass = document.getElementById("pass_input").value
+    const pass = document.getElementById("pass_input").value;
+    const url = "http://localhost:5255/";
 
-    if (!(email === "" || pass.value === "")){
-        const response = await fetch("http://localhost:8080")
+    if (!(email === "" || pass === "")){
+        const response = await fetch(`${url}login`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {
+                "Email": email,
+                "PasswordHash": pass
+            }
+        }).then(response => response.json());
+        if(response.ok){
+            console.log(response);
+        }
+        console.log('cuuuuuuuuuuuuuuuuuuuuuuu');
     }
 }
