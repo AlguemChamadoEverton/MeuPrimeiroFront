d4f91f69dcffdf01ca3cbee25260472bf4603e57
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-03-16 07:12:39 -0300

    Fetch funcional, dando inicio ao armazenamento de jwt token

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-async function loginFetch(){
+import Cookies from "universal-cookie";
 
+export default async function loginFetch() {
+    const cookies = new Cookies();
     const email = document.getElementById("email_input").value.toLowerCase();
     const pass = document.getElementById("pass_input").value;
     const url = "http://localhost:5255/";
 
-    if (!(email === "" || pass === "")){
-        const response = await fetch(`${url}login`,{
+    if (!(email === "" || pass === "")) {
+        const response = await fetch(`${url}login`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: {
+            body: JSON.stringify({
                 "Email": email,
                 "PasswordHash": pass
-            }
-        }).then(response => response.json());
-        if(response.ok){
-            console.log(response);
+            })
+        })
+        if (response.ok) {
+            console.log(response.json());
+            cookies.set("jwt_authorization",)
+        } else {
+            console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
         }
-        console.log('cuuuuuuuuuuuuuuuuuuuuuuu');
     }
 }
