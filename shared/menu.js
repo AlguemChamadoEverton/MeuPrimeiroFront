35f7b40551cf3c3c1a793bd83b4357e86c8a437a
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-04-27 07:33:29 -0300

    Iniciando funcionalidades do menu pesquisar

--- shared/menu.js
+++ shared/menu.js
@@ -1,4 +1,8 @@
 const nav_buttons = document.getElementsByClassName("nav_button");
+let search_result = document.getElementsByClassName("results");
+let search_users = document.getElementById("search_user_global;")
+
+search_users.addEventListener("input", searchUsers)
 
 for (let i = 0; i < nav_buttons.length; i++){
     let value = nav_buttons[i];
@@ -9,6 +13,29 @@ for (let i = 0; i < nav_buttons.length; i++){
     }
 }
 
+export async function searchUsers(event) {
+    let input = event.currentTarget.length;
+    if (input >= 3) {
+        const url = "http://localhost:5255/";
+        //Lança a query pro backend, para cada resultado do meu json criar um elemento div, se não tiver nenhum, retorna cant find
+        const response = await fetch(`${url}users`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "Username": input.value.toString(),
+            })
+
+        })
+        if(response.ok){
+            let data = await response.json();
+            data.users.forEach{
+                
+            }
+        }
+    }
+}
 export function menuHoverChangeColor(event){
     let hoveredElement = event.currentTarget;
     hoveredElement.style.backgroundColor = "rgba(39,39,39,0.29)"
