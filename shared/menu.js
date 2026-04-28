3ca9109310cbb447daa3ce70a44835e8e4adcd77
alguemchamadoeverton <vrtnrdrgs@gmail.com>
2026-04-28 06:56:41 -0300

    Funcionalidades do menu pesquisar v2

--- shared/menu.js
+++ shared/menu.js
@@ -1,12 +1,12 @@
 const nav_buttons = document.getElementsByClassName("nav_button");
 let search_result = document.getElementsByClassName("results");
-let search_users = document.getElementById("search_user_global;")
+let search_users = document.getElementById("search_user_global");
+
 
 search_users.addEventListener("input", searchUsers)
 
 for (let i = 0; i < nav_buttons.length; i++){
     let value = nav_buttons[i];
-    console.log(value.style.color);
     if(value.style.color !== "rgb(30, 144, 255)"){
         value.addEventListener("mouseover", menuHoverChangeColor);
         value.addEventListener("mouseleave", menuBackToDefault)
@@ -14,26 +14,39 @@ for (let i = 0; i < nav_buttons.length; i++){
 }
 
 export async function searchUsers(event) {
-    let input = event.currentTarget.length;
-    if (input >= 3) {
+    let input = event.currentTarget;
+    let result_box = document.getElementById("search_result");
+    if (input.value.length > 2) {
+        let nameSearched = input.value.toString()
+        result_box.style.visibility = "visible";
         const url = "http://localhost:5255/";
         //Lança a query pro backend, para cada resultado do meu json criar um elemento div, se não tiver nenhum, retorna cant find
         const response = await fetch(`${url}users`, {
-            method: "GET",
+            method: "POST",
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "Username": input.value.toString(),
+                "Username": nameSearched,
             })
-
         })
+        let data = await response.json();
         if(response.ok){
-            let data = await response.json();
-            data.users.forEach{
-                
-            }
+            data.users.forEach(user => {
+                //quero que crie uma string com base no template e complete as informações
+            })
         }
+        else if(response.status === 404){
+            let cNameSearched = document.getElementById("tpl_not_found").content.cloneNode(true);
+            cNameSearched.children[0].textContent = `Can't find "${nameSearched}"`
+            result_box.append(cNameSearched);
+        }
+        else{
+            console.log("AAAAAAAAAAAAAAAAAAAAA");
+        }
+    }
+    else{
+        result_box.style.visibility = "hidden";
     }
 }
 export function menuHoverChangeColor(event){
